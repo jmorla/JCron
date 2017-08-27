0045c5c468599a94ef72cc31855e040d9ed1f591
jmorla <jorgemorlapro@gmail.com>
2017-08-27 13:19:19 -0400

    change test

--- src/org/jcron/test/MisTareas.java
+++ src/org/jcron/test/MisTareas.java
@@ -4,7 +4,7 @@ package org.jcron.test;
 import org.jcron.annotations.Cron;
 import org.jcron.annotations.Task;
 import org.jcron.core.JCron;
-
+import java.util.Date;
 /**
  *
  * @author jmorla
@@ -13,9 +13,9 @@ import org.jcron.core.JCron;
 public class MisTareas {
 
 
-    @Task(name = "hola", seconds = 1,minute = 5,hour = 3,dayOfWeek = 5,dayOfMonth = 25,month = -1)
+    @Task(name = "hello", seconds = 1,minute = 5,hour = 3,dayOfWeek = 5,dayOfMonth = 25,month = -1)
     public void hola(){
-        System.out.println("Hola palomo");
+        System.out.println("Hello world, I will execute in"+new Date());
     }
     
     public static void main(String[] args) {
